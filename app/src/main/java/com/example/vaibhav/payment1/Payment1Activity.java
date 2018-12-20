b085b036be8e807eba3bd59a874d84172128f29e
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-20 11:10:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/payment1/Payment1Activity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.payment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payment1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment1);
+    }
+}
